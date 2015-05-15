e043e9cc659918e021df8ce797fffff60886564b
Wedika Jadhav <vedikajadhav48@gmail.com>
2015-05-14 22:28:11 -0700

    All Login Screens UI

--- app/src/main/java/com/example/vedikajadhav/tieinn/LoginActivity.java
+++ app/src/main/java/com/example/vedikajadhav/tieinn/LoginActivity.java
@@ -36,8 +36,8 @@ public class LoginActivity extends ActionBarActivity {
     private static final String TAG= "LoginActivity";
     private EditText mUserNameEditText = null;
     private EditText mPasswordEditText = null;
-   // private String username;
-    //private String password;
+    private String username;
+    private String password;
     private TextView mSignUpTextView;
     private Button mLoginButton;
     private LoginButton mFacebookLoginButton;
@@ -66,8 +66,8 @@ public class LoginActivity extends ActionBarActivity {
         Log.i(TAG, "Login through tieIn app");
         //if((!userNameEditText.getText().toString().equals("")) && (!passwordEditText.getText().toString().equals(""))){
           if(true){
-            NetAsync(button);
-          //  loginHandler.post(new LoginThread());
+           // NetAsync(button);
+            mLoginHandler.post(new LoginThread());
         }else if(mUserNameEditText.getText().toString().equals("")){
             Toast.makeText(getApplicationContext(),
                     "Username field empty", Toast.LENGTH_SHORT).show();
@@ -98,147 +98,7 @@ public class LoginActivity extends ActionBarActivity {
 
         @Override
         public void run() {
-
+            Log.i(TAG, "Logn Thread getting executed");
         }
-    }
-
-    /**
-     * Async Task to check whether internet connection is working.
-     **/
-
-    private class NetCheck extends AsyncTask<String,String,Boolean>
-    {
-        private ProgressDialog nDialog;
-
-        @Override
-        protected void onPreExecute(){
-            super.onPreExecute();
-            nDialog = new ProgressDialog(LoginActivity.this);
-            nDialog.setTitle("Checking Network");
-            nDialog.setMessage("Loading..");
-            nDialog.setIndeterminate(false);
-            nDialog.setCancelable(true);
-            nDialog.show();
-        }
-        /**
-         * Gets current device state and checks for working internet connection by trying Google.
-         **/
-        @Override
-        protected Boolean doInBackground(String... args){
-
-
-
-            ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo netInfo = cm.getActiveNetworkInfo();
-            if (netInfo != null && netInfo.isConnected()) {
-                try {
-                    URL url = new URL("http://www.google.com");
-                    HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
-                    urlc.setConnectTimeout(3000);
-                    urlc.connect();
-                    if (urlc.getResponseCode() == 200) {
-                        return true;
-                    }
-                } catch (MalformedURLException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-            return false;
-
-        }
-        @Override
-        protected void onPostExecute(Boolean th){
-
-            if(th == true){
-                nDialog.dismiss();
-                new ProcessLogin().execute();
-            }
-            else{
-                nDialog.dismiss();
-                //loginErrorMsg.setText("Error in Network Connection");
-            }
-        }
-    }
-
-    /**
-     * Async Task to get and send data to My Sql database through JSON respone.
-     **/
-    private class ProcessLogin extends AsyncTask<String, String, JSONObject> {
-
-
-        private ProgressDialog pDialog;
-
-        String username,password;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            mUserNameEditText = (EditText) findViewById(R.id.edit_text_username_to_login);
-            mPasswordEditText = (EditText) findViewById(R.id.edit_text_password_to_login);
-            username = mUserNameEditText.getText().toString();
-            password = mPasswordEditText.getText().toString();
-            pDialog = new ProgressDialog(LoginActivity.this);
-            pDialog.setTitle("Contacting Servers");
-            pDialog.setMessage("Logging in ...");
-            pDialog.setIndeterminate(false);
-            pDialog.setCancelable(true);
-            pDialog.show();
-        }
-
-        @Override
-        protected JSONObject doInBackground(String... args) {
-
-            UserFunctions userFunction = new UserFunctions();
-            JSONObject json = userFunction.loginUser(username, password);
-            return json;
-        }
-
-        @Override
-        protected void onPostExecute(JSONObject json) {
-            try {
-                if (json.getString(KEY_SUCCESS) != null) {
-
-                    String res = json.getString(KEY_SUCCESS);
-
-                    if(Integer.parseInt(res) == 1){
-                        pDialog.setMessage("Loading User Space");
-                        pDialog.setTitle("Getting Data");
-                        DatabaseHandler db = new DatabaseHandler(getApplicationContext());
-                        JSONObject json_user = json.getJSONObject("user");
-                        /**
-                         * Clear all previous data in SQlite database.
-                         **/
-                        UserFunctions logout = new UserFunctions();
-                        logout.logoutUser(getApplicationContext());
-                       // db.addUser(json_user.getString(KEY_FIRSTNAME),json_user.getString(KEY_LASTNAME),json_user.getString(KEY_EMAIL),json_user.getString(KEY_USERNAME),json_user.getString(KEY_UID),json_user.getString(KEY_CREATED_AT));
-                        /**
-                         *If JSON array details are stored in SQlite it launches the User Panel.
-                         **/
-                        Intent upanel = new Intent(getApplicationContext(), Home.class);
-                        upanel.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        pDialog.dismiss();
-                        startActivity(upanel);
-                        /**
-                         * Close Login Screen
-                         **/
-                        finish();
-                    }else{
-
-                        pDialog.dismiss();
-                        //loginErrorMsg.setText("Incorrect username/password");
-                    }
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-    public void NetAsync(View view){
-        new NetCheck().execute();
     }
 }
--- app/src/main/java/com/example/vedikajadhav/tieinnLibrary/UserFunctions.java
+++ app/src/main/java/com/example/vedikajadhav/tieinnLibrary/UserFunctions.java
@@ -1,14 +1,5 @@
 package com.example.vedikajadhav.tieinnLibrary;
 
-import android.content.Context;
-
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by Vedika Jadhav on 5/14/2015.
  */
@@ -18,87 +9,5 @@ public class UserFunctions {
 
         //URL of the PHP API
         private static String loginURL = "http://tiein.comyr.com/api.php?";
-        private static String registerURL = "http://tiein.comyr.com/api.php?";
-        private static String forpassURL = "http://tiein.comyr.com/api.php?";
-        private static String chgpassURL = "http://tiein.comyr.com/api.php?";
-
-
-        private static String login_tag = "login";
-        private static String register_tag = "register";
-        private static String forpass_tag = "forpass";
-        private static String chgpass_tag = "chgpass";
-
-
-        // constructor
-        public UserFunctions(){
-            jsonParser = new JSONParser();
-        }
-
-        /**
-         * Function to Login
-         **/
-
-        public JSONObject loginUser(String username, String password){
-            // Building Parameters
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("tag", login_tag));
-            params.add(new BasicNameValuePair("email", username));
-            params.add(new BasicNameValuePair("password", password));
-            JSONObject json = jsonParser.getJSONFromUrl(loginURL, params);
-            return json;
-        }
-
-        /**
-         * Function to change password
-         **/
-
-        public JSONObject chgPass(String newpas, String email){
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("tag", chgpass_tag));
-
-            params.add(new BasicNameValuePair("newpas", newpas));
-            params.add(new BasicNameValuePair("email", email));
-            JSONObject json = jsonParser.getJSONFromUrl(chgpassURL, params);
-            return json;
-        }
-
-        /**
-         * Function to reset the password
-         **/
-
-        public JSONObject forPass(String forgotpassword){
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("tag", forpass_tag));
-            params.add(new BasicNameValuePair("forgotpassword", forgotpassword));
-            JSONObject json = jsonParser.getJSONFromUrl(forpassURL, params);
-            return json;
-        }
-
-        /**
-         * Function to  Register
-         **/
-        public JSONObject registerUser(String fname, String lname, String email, String uname, String password){
-            // Building Parameters
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("tag", register_tag));
-            params.add(new BasicNameValuePair("fname", fname));
-            params.add(new BasicNameValuePair("lname", lname));
-            params.add(new BasicNameValuePair("email", email));
-            params.add(new BasicNameValuePair("uname", uname));
-            params.add(new BasicNameValuePair("password", password));
-            JSONObject json = jsonParser.getJSONFromUrl(registerURL,params);
-            return json;
-        }
-
-
-        /**
-         * Function to logout user
-         * Resets the temporary data stored in SQLite Database
-         * */
-        public boolean logoutUser(Context context){
-            DatabaseHandler db = new DatabaseHandler(context);
-            db.resetTables();
-            return true;
-        }
 
     }
