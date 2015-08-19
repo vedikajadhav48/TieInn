410aa132faf713951cb8b2cfb0f61fa18352b121
Wedika Jadhav <vedikajadhav48@gmail.com>
2015-08-18 21:29:40 -0700

    FacebookLoginWorking with profile picture

--- app/src/main/java/com/example/vedikajadhav/tieinn/LoginActivity.java
+++ app/src/main/java/com/example/vedikajadhav/tieinn/LoginActivity.java
@@ -20,8 +20,13 @@ import android.widget.Toast;
 
 import com.example.vedikajadhav.tieinnLibrary.DatabaseHandler;
 import com.example.vedikajadhav.tieinnLibrary.UserFunctions;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -46,20 +51,56 @@ public class LoginActivity extends ActionBarActivity {
     private static final int Intent_User_Index = 123;
     private static String KEY_SUCCESS = "message";
 
+    private TextView info;
+    private CallbackManager callbackManager;
+    private ProfilePictureView profilePictureView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_login);
         mUserNameEditText = (EditText)findViewById(R.id.edit_text_username_to_login);
         mPasswordEditText = (EditText)findViewById(R.id.edit_text_password_to_login);
        // username = userNameEditText.getText().toString();
        // password = passwordEditText.getText().toString();
+        info = (TextView)findViewById(R.id.info);
         mLoginButton = (Button)findViewById(R.id.login_button);
         mFacebookLoginButton = (LoginButton)findViewById(R.id.facebook_login_button);
         mSignUpTextView = (TextView)findViewById(R.id.sign_up_text_view);
         mForgotPasswordTextView = (TextView)findViewById(R.id.forgot_password_text_view);
         //facebook_login_button.setText("Log In");
+        profilePictureView = (ProfilePictureView) findViewById(R.id.image);
+
+        mFacebookLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.i(TAG, "onSuccess");
+                /*info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );
+                profilePictureView.setProfileId(loginResult.getAccessToken().getUserId());*/
+                Intent createAccountIntent = new Intent(getApplicationContext(), Home.class);
+                startActivityForResult(createAccountIntent, Intent_User_Index);
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i(TAG, "onCancel");
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Log.i(TAG, "onError");
+                info.setText("Login attempt failed.");
+            }
+        });
     }
 
     public void login(View button){
@@ -89,9 +130,14 @@ public class LoginActivity extends ActionBarActivity {
         Log.i(TAG, "Forgot Password");
     }
 
-    @Override
+   /* @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+    }*/
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     private class LoginThread implements Runnable{
