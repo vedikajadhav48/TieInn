a5c31bca2b1bf4347acbaf10b70cd72e0c536f9a
Wedika Jadhav <vedikajadhav48@gmail.com>
2015-09-18 08:39:33 -0700

    write answer dialog working

--- app/src/main/java/com/example/vedikajadhav/tieinnLibrary/DiscussionListAdapter.java
+++ app/src/main/java/com/example/vedikajadhav/tieinnLibrary/DiscussionListAdapter.java
@@ -28,19 +28,15 @@ import java.util.zip.Inflater;
 public class DiscussionListAdapter extends ArrayAdapter<DiscussionItem>{
     private static final String TAG= "DiscussionListAdapter";
     private final Activity mContext;
-    private LayoutInflater mLayoutInflator;
     private final ArrayList<DiscussionItem> mDiscussionItemList;
     private Button answerButton = null;
-    private ButtonClickListener mButtonClickListener;
     AnswerListAdapter answerListAdapter;
     private ArrayList<AnswerItem> mHousingAnswerList = new ArrayList<>();
 
     public DiscussionListAdapter(ArrayList<DiscussionItem> discussionItemList, Activity context) {
         super(context, 0, discussionItemList);
         this.mContext = context;
-       // mLayoutInflator = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.mDiscussionItemList = discussionItemList;
-       // mButtonClickListener = buttonClickListener;
     }
 
 /*    @Override
@@ -60,22 +56,9 @@ public class DiscussionListAdapter extends ArrayAdapter<DiscussionItem>{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View row=convertView;
-        YourWrapper wrapper = null;
-
         if(convertView==null) {
             convertView = (mContext).getLayoutInflater().inflate(R.layout.discussion_board_list_row, null, true);
         }
-        /*if(row==null){
-            //convertView = ((Activity)mContext).getLayoutInflater().inflate(R.layout.discussion_board_list_row, null);
-            row = ((Activity)mContext).getLayoutInflater().inflate(R.layout.discussion_board_list_row, null);
-            wrapper = new YourWrapper(row);
-            row.setTag(wrapper);
-        }
-        else{
-            wrapper = (YourWrapper)row.getTag();
-        }*/
-
 
        // ImageView thumbImage=(ImageView)row.findViewById(R.id.discussion_board_question_image);
         TextView categoryTextView = (TextView)convertView.findViewById(R.id.discussion_board_question_category);
@@ -90,9 +73,6 @@ public class DiscussionListAdapter extends ArrayAdapter<DiscussionItem>{
 
             @Override
             public void onClick(View v) {
-                /*if(mButtonClickListener != null){
-                    mButtonClickListener.onButtonClick((Integer)v.getTag());
-                }*/
                 final Dialog writeAnswerDialog = new Dialog(mContext, R.style.FullHeightDialog);
                 writeAnswerDialog.setContentView(R.layout.write_answer_dialog);
                 writeAnswerDialog.setCancelable(true);
@@ -111,26 +91,6 @@ public class DiscussionListAdapter extends ArrayAdapter<DiscussionItem>{
             }
         });
 
-       /* wrapper.getButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final Dialog writeAnswerDialog = new Dialog(getContext(), R.style.FullHeightDialog);
-                writeAnswerDialog.setContentView(R.layout.write_answer_dialog);
-                writeAnswerDialog.setCancelable(true);
-                writeAnswerDialog.show();
-
-                Button submitButton = (Button) writeAnswerDialog.findViewById(R.id.dialog_answer_submit_button);
-                submitButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        EditText editText = (EditText) writeAnswerDialog.findViewById(R.id.edit_text_answer);
-                        String answer = editText.getText().toString();
-                        //postInstructorComment(comment);
-                        writeAnswerDialog.dismiss();
-                    }
-                });
-            }
-        });*/
        /* for(int i=0; i<5; i++){
             AnswerItem newAnswerItem = new AnswerItem();
             newAnswerItem.setAnswerItemText("Starbucks is the favourite coffee place");
